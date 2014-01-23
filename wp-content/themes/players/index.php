8194c38cd1e159027f82dcccbfcc47f939b03ac9
Richard Caudle <richard.caudle@datasift.com>
2014-01-23 21:04:13 +0000

    Final tweaks to styling for launch

--- wp-content/themes/players/index.php
+++ wp-content/themes/players/index.php
@@ -9,19 +9,19 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <div class="row">
-    <div class="col-sm-8 feature news">
+    <div class="col-sm-12 feature news">
       <a class="cta2" href="news">
         <span class="title">News</span>
         <span class="follow">&gt;</span>
       </a>
       <?php get_template_part('templates/content', get_post_format()); ?>
     </div>
-    <div class="col-sm-4 feature twitter">
+    <!-- <div class="col-sm-4 feature twitter">
      <a class="cta2" href="http://twitter.com/LeckyPlayers">
         <span class="title">Twitter</span>
         <span class="follow">&gt;</span>
       </a>
-     <?php dynamic_sidebar('sidebar-primary'); ?>
-    </div>
+     // <?php dynamic_sidebar('sidebar-primary'); ?>
+    </div> -->
   </div>
 <?php endwhile; ?>
