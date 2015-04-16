976abb69ef74ee2f4bc2e0c8460e6a93eef06183
tribey2 <rob@robtribe.co.uk>
2015-04-16 18:09:54 +0100

    Change Facebook Link To New Page
    Change Facebook Link To New Page

--- wp-content/themes/players/templates/header-top-navbar.php
+++ wp-content/themes/players/templates/header-top-navbar.php
@@ -2,7 +2,7 @@
   <div class="container">
     <a id="logo" href="/" title="The Leckhampton Players"><img src="/images/logo.jpg" /></a>
     <div id="social">
-      <a href="https://www.facebook.com/groups/2390514148/" title="Facebook"><img src="/images/facebook.png" alt="Facebook" /></a>
+      <a href="https://www.facebook.com/leckhamptonplayers/" title="Facebook"><img src="/images/facebook.png" alt="Facebook" /></a>
       <a href="https://twitter.com/LeckyPlayers" title="Twitter"><img src="/images/twitter.png" alt="Twitter" /></a>
     </div>
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
