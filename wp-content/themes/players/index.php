5d9b229fbebed52f7a456acb0d60fc01f8707fb8
tribey2 <rob@robtribe.co.uk>
2015-10-11 23:25:25 +0100

    Update Facebook Like-Box Version

--- wp-content/themes/players/index.php
+++ wp-content/themes/players/index.php
@@ -13,10 +13,9 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
   fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
+}(document, 'script', 'facebook-jssdk'));</script>
 
 <?php while (have_posts()) : the_post(); ?>
   <div class="row">
--- wp-content/themes/players/templates/facebook-like-box.php
+++ wp-content/themes/players/templates/facebook-like-box.php
@@ -2,6 +2,6 @@
         <h3 class="entry-title"></h3>
     </header>
     <div align="center">
-        <div class="fb-like-box" data-href="https://www.facebook.com/leckhamptonplayers" data-width="100%" data-height="450" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true">
+        <div class="fb-page" data-href="https://facebook.com/leckhamptonplayers" data-width="500" data-height="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://facebook.com/leckhamptonplayers"><a href="https://facebook.com/leckhamptonplayers">The Leckhampton Players</a></blockquote></div></div>
         </div>
   </div>
