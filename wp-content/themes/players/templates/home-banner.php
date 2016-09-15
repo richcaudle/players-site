6dc3bce1093e34258b3a16f0d3a7c3ef076c9098
Rob Tribe <rob@robtribe.co.uk>
2016-09-15 22:01:47 +0100

    Update banner

--- wp-content/themes/players/templates/home-banner.php
+++ wp-content/themes/players/templates/home-banner.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-xs-12 carousel">
       <a href="next-production" style='width:100%'>
-        <img src="images/ripper-banner.jpg" />
+        <img src="images/murder-on-cue-banner.jpg" />
       </a>
     </div>
   </div>
