1cadb030e30352cb4087cd05f130c3112cf5e150
Rob Tribe <rob@robtribe.co.uk>
2016-10-30 13:03:34 +0000

    Update index.php

--- wp-content/themes/players/index.php
+++ wp-content/themes/players/index.php
@@ -19,7 +19,7 @@
 
   <div class="row">
     <div class="col-sm-6 feature news">
-      <a class="cta2" href="http://www.leckhamptonplayers.co.uk">
+      <a class="cta2" href="http://www.leckhamptonplayers.com">
         <span class="title">Next production</span>
         <span class="follow">&gt;</span>
       </a>
