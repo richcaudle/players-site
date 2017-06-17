db7c1786edbfcc526bca318a9de60db6e5d7cd30
Rob Tribe <rob@robtribe.co.uk>
2017-06-17 15:21:52 +0100

    Update 'next production' text

--- wp-content/themes/players/index.php
+++ wp-content/themes/players/index.php
@@ -20,14 +20,12 @@
   <div class="row">
     <div class="col-sm-6 feature news">
       <a class="cta2" href="http://www.leckhamptonplayers.com">
-        <span class="title">Next Production - Tickets Now On Sale!</span>
+        <span class="title">Next Production</span>
         <span class="follow">&gt;</span>
       </a>
       <div style="padding: 10px;">
-        <p>Book your tickets now for 'Old Time Music Hall' - our next production which promises to be an evening packed with music, melodrama and laughter, and runs from 30th May - 3rd June 2017.</p>
-        <p>Tickets are now on sale and cost just £10 (including ploughmans supper), a licenced bar is also available.</p>
-        <p>Call the box office now on 07867 884 985 (lines open 10am - 9pm daily) or <a href="http://www.tribehost.co.uk/tickets.php">book your tickets online</a> before it's too late!</p>
-        <p><strong><a href="http://www.tribehost.co.uk/tickets.php">Book Tickets &rarr;</a></strong></p>
+        <p>Our next product is the autumn play and will run from 4th - 7th October 2017.</p>
+        <p>Check back soon for more information, and how you can book your tickets.</p>
       </div>
     </div>
     <!-- <div class="col-sm-4 feature twitter">
